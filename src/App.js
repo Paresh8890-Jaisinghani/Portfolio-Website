35eee8e180dd827c004ee790c6b152bd3a60040b
Paresh8890-Jaisinghani <136781047+Paresh8890-Jaisinghani@users.noreply.github.com>
2023-08-25 22:23:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/navbar';
+import Home from './components/home';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
